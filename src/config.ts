61c94df2ed4fb93686d3fb0b80d9ecf906fc6d47
vtching <valerie.tching@gmail.com>
2025-01-23 12:34:08 -0800

    Update config.ts to use template name

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,11 @@ const AppConfig = {
   jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret',
   port: parseInt(process.env.API_PORT || '8080'),
   postgres: {
-    db: process.env.DATABASE_NAME || 'magellan',
+    db: process.env.DATABASE_NAME || 'myproject',
     port: parseInt(process.env.DATABASE_PORT || '5432'),
-    host: process.env.DATABASE_HOST || 'magellan-db',
-    user: process.env.DATABASE_USER || 'magellan',
-    passwd: process.env.DATABASE_PASSWORD || 'magellanpwd',
+    host: process.env.DATABASE_HOST || 'myproject-db',
+    user: process.env.DATABASE_USER || 'myproject',
+    passwd: process.env.DATABASE_PASSWORD || 'myprojectpwd',
     // ssl: 
     // ssl_ca_cert: 
   },
